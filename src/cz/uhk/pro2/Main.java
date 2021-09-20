78767d15c1a87fbe74e8aa9f4e6d2ba76ca58962
moskata1 <moskata1@uhk.cz>
2021-09-20 14:42:21 +0200

    naplneni menu

--- src/cz/uhk/pro2/Main.java
+++ src/cz/uhk/pro2/Main.java
@@ -1,8 +1,20 @@
 package cz.uhk.pro2;
 
+import models.PizzaMenu;
+import models.PizzaMenuItem;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        PizzaMenu menu = new PizzaMenu();
+        menu.additem(new PizzaMenuItem("Hawai", "Ananas", price 120));
+        menu.equals(new PizzaMenuItem("sunovka", "sunka", 150));
+        System.out.println("last update: );
+
+
+        for (PizzaMenuItem item : menu.getItems()) {
+            System.out.println(item);
+        }
     }
 }
---
+++ src/models/PizzaMenu.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.time.DateTimeException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaMenu {
+    private List PizzaMenuItem Items;
+    private int LocalDateTime lastUpdate;
+    public PizzaMenu();
+        items = new ArrayList PizzaMenu ();
+
+    }
+    public List PizzaMenuItem getItems()
+    public void additem(PizzaMenuItem Item){
+        items.add(item);
+        lastUpdate = LocalDateTime.now();
+    }
+
+    public List getPizzaMenuItem() {
+        return PizzaMenuItem;
+    }
+
+    public int getLocalDateTime() {
+        return LocalDateTime;
+    }
+}
